6c3db0bbf1f860d41c45b23a032878b85fd2ed3e
ChKarishan <karishan.897@gmail.com>
2023-09-22 23:21:10 +0500

    tasks1-6

--- final_project/index.js
+++ final_project/index.js
@@ -1,33 +1,8 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const session = require('express-session')
-//const customer_routes = require('./router/auth_users.js').authenticated;
-//const genl_routes = require('./router/general.js').general;
-
-let users = []
-
-const doesExist = (username)=>{
-    let userswithsamename = users.filter((user)=>{
-      return user.username === username
-    });
-    if(userswithsamename.length > 0){
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  //Function to check if the user is authenticated
-const authenticatedUser = (username,password)=>{
-    let validusers = users.filter((user)=>{
-      return (user.username === username && user.password === password)
-    });
-    if(validusers.length > 0){
-      return true;
-    } else {
-      return false;
-    }
-  }
+const customer_routes = require('./router/auth_users.js').authenticated;
+const genl_routes = require('./router/general.js').general;
 
 const app = express();
 
@@ -52,53 +27,10 @@ if(req.session.authorization) { //get the authorization object stored in the ses
      return res.status(403).json({message: "User not logged in"})
  }
 });
-
-
-app.post("/register", (req,res) => {
-    const username = req.query.username;
-    const password = req.query.password;
-  
-    if (username && password) {
-      if (!doesExist(username)) { 
-        users.push({"username":username,"password":password});
-        return res.status(200).json({message: "User successfully registred. Now you can login"});
-      } else {
-        return res.status(404).json({message: "User already exists!"});    
-      }
-    } 
-    return res.status(404).json({message: "Unable to register user."});
-  });
-
-  app.post("/login", (req,res) => {
-    const username = req.query.username;
-    const password = req.query.password;
-  
-    if (!username || !password) {
-        return res.status(404).json({message: "Error logging in"});
-    }
-  
-    if (authenticatedUser(username,password)) {
-      let accessToken = jwt.sign({
-        data: password
-      }, 'access', { expiresIn: 60 * 60 });
-  
-      req.session.authorization = {
-        accessToken,username
-    }
-    return res.status(200).send("User successfully logged in");
-    } else {
-      return res.status(208).json({message: "Invalid Login. Check username and password"});
-    }
-  });
-
-// just to verify user login is done ;)
-  app.get("/customer/auth/loginDetails",(req,res)=>{
-    res.send(JSON.stringify(loginDetails));
-});
  
 const PORT =5000;
 
-//app.use("/customer", customer_routes);
-//app.use("/", genl_routes);
+app.use("/customer", customer_routes);
+app.use("/", genl_routes);
 
 app.listen(PORT,()=>console.log("Server is running"));
